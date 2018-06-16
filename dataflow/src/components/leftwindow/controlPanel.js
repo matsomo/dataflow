422e76b24a025a16aad94ba2bb60688090358da9
Mats Aadahl <matsaadahl@gmail.com>
2018-06-16 19:20:05 +0200

    styled component

--- dataflow/src/components/leftwindow/controlPanel.js
+++ dataflow/src/components/leftwindow/controlPanel.js
@@ -4,13 +4,14 @@ import styled from 'styled-components';
 class controlPanel extends React.Component {
   const Title = styled.div`
      font-size: 16px;
+     color: black;
   `;
 
 
   render(){
     return(
       <div>
-
+        <Title>Left Window</Title>
       </div>
     );
   }
