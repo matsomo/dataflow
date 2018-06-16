d005572b22558ed95e3ebfc424ac2b4ee332d928
Mats Aadahl <matsaadahl@gmail.com>
2018-06-16 21:14:44 +0200

    searchfield not iheritting border radius

--- dataflow/src/App.js
+++ dataflow/src/App.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
 import LeftControlPanel from './components/leftwindow/leftControlPanel';
 import LeftOutput from './components/leftwindow/leftOutput';
+import styled from 'styled-components';
 import './App.css';
 
+const Window = styled.div`
+  background-color: #262728;
+  margin: 0;
+  padding: 0;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+`;
+
+const LeftContainer = styled.div`
+  background-color: #46494c;
+  height: 100%;
+  width: 50%;
+  padding: 20px;
+  margin: 0;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h3>app</h3>
-        <div>
+      <Window>
+        <LeftContainer>
           <LeftControlPanel />
           <LeftOutput />
-        </div>
-      </div>
+        </LeftContainer>
+      </Window>
     );
   }
 }
--- dataflow/src/components/leftwindow/leftControlPanel.js
+++ dataflow/src/components/leftwindow/leftControlPanel.js
@@ -1,11 +1,51 @@
 import React, {Component} from 'react';
+import styled from 'styled-components';
+
+const MainPanel = styled.div`
+  text-align: center;
+`;
+
+const SearchField = styled.input`
+  height: 35px;
+  width: 400px;
+  border: 0;
+  outline: 0;
+  padding-left: 35px;
+  :focus {
+    outline: 0;
+  }
+`;
+
+const SearchButton = styled.button`
+  height: 37px;
+  border: 0;
+  width: 60px;
+  margin: 0;
+  padding: 4px;
+`;
+
+const SearchBar = styled.div`
+  border-radius: 15px;
+  width: 460px;
+  margin: auto;
+  padding: 0;
+  background-color: blue;
+  overflow: hidden;
+`;
+
+const ButtonPanel = styled.div`
+  text-align: center;
+`;
 
 class LeftControlPanel extends Component {
   render() {
     return(
-      <div>
-        <h3>left panel</h3>
-      </div>
+      <MainPanel>
+        <SearchBar>
+          <SearchField placeholder='What are you looking for?' type='text'/>
+          <SearchButton>Search</SearchButton>
+        </SearchBar>
+      </MainPanel>
     );
   }
 }
